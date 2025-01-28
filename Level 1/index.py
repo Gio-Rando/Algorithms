2ccafdc03ff4162b11ae2192cd7c5e3e786a015f
unknown <giorgi200824@gmail.com>
2025-01-28 20:47:12 +0400

    comitet

--- Level 1/index.py
+++ Level 1/index.py
@@ -13,6 +13,51 @@ def task2(a):
     return binary
 print(task2(10))
 
-def task3(a):
-    
-    
+def task3(a,b,c,d,e):
+    if a > b:
+        if a > c:
+            if a > d:
+                if a > e:
+                    return "a"
+                else:
+                    return "e"
+            else:
+                if d > e:
+                    return "d"
+                else:
+                    return "e"
+        else:
+            if c > d:
+                if c > e:
+                    return "c"
+                else:
+                    return "e"
+            else:
+                if d > e:
+                    return "d"
+                else:
+                    return "e"
+    else:
+        if b > c:
+            if b > d:
+                if b > e:
+                    return "b"
+                else:
+                    return "e"
+            else:
+                if d > e:
+                    return "d"
+                else:
+                    return "e"
+        else:
+            if c > d:
+                if c > e:
+                    return "c"
+                else:
+                    return "e"
+            else:
+                if d > e:
+                    return "d"
+                else:
+                    return "e"
+                
